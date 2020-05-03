d7d617d39c9a755ca0dc9771a4e10039f00f7e45
daring <danila.daring@gmail.com>
2020-05-03 14:57:59 +0300

    add db scripts for BaseTestEntity

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -142,3 +142,20 @@ create table LIBRARY_BOOK_AUTHOR_LINK (
     primary key (BOOK_ID, AUTHOR_ID)
 )^
 -- end LIBRARY_BOOK_AUTHOR_LINK
+-- begin LIBRARY_BASE_TEST_ENTITY
+create table LIBRARY_BASE_TEST_ENTITY (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    DTYPE varchar(31),
+    --
+    TOWN_ID varchar(36),
+    --
+    primary key (ID)
+)^
+-- end LIBRARY_BASE_TEST_ENTITY
---
+++ modules/core/db/update/hsql/20/200503-1-createBaseTestEntity.sql
@@ -0,0 +1,15 @@
+create table LIBRARY_BASE_TEST_ENTITY (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    DTYPE varchar(31),
+    --
+    TOWN_ID varchar(36),
+    --
+    primary key (ID)
+);
